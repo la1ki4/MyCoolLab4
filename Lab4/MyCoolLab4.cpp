7fed06e293744405832420901f9d6316c9ba34fb
la1ki4 <100294709+la1ki4@users.noreply.github.com>
2023-05-13 18:21:51 +0300

    Update MyCoolLab4.cpp

--- Lab4/MyCoolLab4.cpp
+++ Lab4/MyCoolLab4.cpp
@@ -5,7 +5,7 @@ int main() {
     int dim_sizes[] = { 3, 4, 5 };
     MultiArray arr(3, dim_sizes);
 
-    // Заполнение массива элементами
+    // Р—Р°РїРѕР»РЅРµРЅРёРµ РјР°СЃСЃРёРІР° СЌР»РµРјРµРЅС‚Р°РјРё
     for (int i = 0; i < dim_sizes[0]; i++) {
         for (int j = 0; j < dim_sizes[1]; j++) {
             for (int k = 0; k < dim_sizes[2]; k++) {
@@ -16,11 +16,11 @@ int main() {
         }
     }
 
-    // Изменение элемента массива
+    // РР·РјРµРЅРµРЅРёРµ СЌР»РµРјРµРЅС‚Р° РјР°СЃСЃРёРІР°
     int indices[] = { 2, 0, 4 };
     arr.setElement(indices, 921);
 
-    // Вывод элемента массива
+    // Р’С‹РІРѕРґ СЌР»РµРјРµРЅС‚Р° РјР°СЃСЃРёРІР°
     std::cout << "Element [" << indices[0] << ", " << indices[1] << ", " << indices[2] << "] = " << arr.getElement(indices) << std::endl;
     std::cout << "Index of Element [" << indices[0] << ", " << indices[1] << ", " << indices[2] << "] = " << arr.getElementsByAyliff(indices) << std::endl;
     std::cout << "Address of Element [" << indices[0] << ", " << indices[1] << ", " << indices[2] << "] = " << arr.getVectors() << std::endl;
